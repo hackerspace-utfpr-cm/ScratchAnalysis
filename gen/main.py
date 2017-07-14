22173c70769259f9fb76e68857e617e5ad61dbc3
VivianSnow <viviansnow@bupt.edu.cn>
2017-07-14 17:22:29 +0800

    更新了sb2解压缩，现在直接提供sb2文件即可
    RT

--- gen/main.py
+++ gen/main.py
@@ -3,9 +3,31 @@ from antlr4 import *
 from AntlrLexer import AntlrLexer
 from AntlrParser import AntlrParser
 from AntlrListener import AntlrListener
+import zipfile
+import os
+import codecs
+
+
+def unzip_scratch(filename):
+    """
+    unzip scratch project and extract project.json file   
+    :param filename: filename fo scratch project 
+    :return: null or project.json content
+    """
+    zfile = zipfile.ZipFile(filename, 'r')
+    if "project.json" in zfile.namelist():
+        data = zfile.read("project.json")
+        return data
+    else:
+        return null
+
 
 def main(argv):
-    input = FileStream(argv[1], encoding='utf-8')
+    raw_json = unzip_scratch(argv[1])
+    encoded_json = codecs.decode(raw_json, 'utf-8', 'strict')
+    input = InputStream(encoded_json)
+    if not input:
+        return
     lexer = AntlrLexer(input)
     stream = CommonTokenStream(lexer)
     parser = AntlrParser(stream)
