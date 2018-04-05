118027c82024c47ae50d357cd5743fbc97b5a0b1
cloudPai <1565730756@qq.com>
2018-04-05 16:56:58 +0800

    2018年04月05日16:56:55

---
+++ gen/test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# file: example1.py
+import string
+#
+# # 这个是 str 的字符串
+s = '关关雎鸠'
+#
+# # 这个是 unicode 的字符串
+u = u'关关雎鸠'
+#
+# print (isinstance(s, str))  # True
+print(s)
+# print(s.__class__)
+
+# print (isinstance(u, unicode))  # True
+#
+# print (s.__class__)  # <type 'str'>
+# print (u.__class__)  # <type 'unicode'>
+
+
+# 输出结果：
+# "cities": [{"cityid": "101010100", "city": "北京"}, {"cityid": "101020100", "city": "上海"}]
